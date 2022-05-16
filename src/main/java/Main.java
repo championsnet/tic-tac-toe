5ac593bb7c85ddf030984481eef4a9240323d58d
Akis Parousis <akis.parousis@gwf-labs.gr>
2022-05-16 12:13:28 +0300

    [Main] App initialization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,103 +2,20 @@ package main.java;
 
 import main.java.model.*;
 import main.java.view.*;
+import main.java.controller.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		PlayerRoster a = new PlayerRoster();
-		Player b = new Player("Harry");
-		a.addPlayer(b);
-		
-		MainWindow view = new MainWindow();
-		
+		// Initialize logic object
 		char[][] initTable = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
-		
 		GameLogic logic = new GameLogic(initTable);
-		
-		Board board = new Board(0, 'T', 'x');
-		int x = 0, y = 0; 
-		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-		
-		if (logic.isFinished(board)) {
-			System.out.println(board.getState() + " is the winner!");
-		}
-		
-		view.getBoardPanel().setCell(0, 0, 1);
-		view.getBoardPanel().setCell(1, 0, 0);
-		view.getBoardPanel().setCell(1, 1, 1);
-		
-		view.getBoardPanel().setMessage("You can change me Akis");
-		
-		
-		
-		
-		
-		
-//		x = 1;
-//		y = 1; 
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		x = 1;
-//		y = 0; 
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		x = 1;
-//		y = 2; 
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		x = 2;
-//		y = 0; 
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-//		
-//		board = new Board(board.getFilledPos()+1, board.getState(), board.nextPlayer());
-//		if (logic.isEmpty(x, y)) logic.move(x, y, board);
-//		
-//		if (logic.isFinished(board)) {
-//			System.out.println(board.getState() + " is the winner!");
-//		}
-
+		// Initialize view object
+		MainWindow view = new MainWindow();
+		// Initialize board object
+		Board board = new Board(0,'T','x');
+		// Initialize Controller object
+		Controller c = new Controller(logic, view, board);
 	}
 
 }
